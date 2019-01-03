e0e01a8434b61873078cd9653190ea7b7fb0a7aa
Sai <saicharan13@gmail.com>
2019-01-03 22:46:38 +0530

    Add search form

--- index.php
+++ index.php
@@ -5,11 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Bickle</title>
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300|Open+Sans" rel="stylesheet">
   <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
   <section class="front-page-wrapper">
-    <h1>Bickle</h1>
+    <h1 class="site-title">Bickle</h1>
+
+    <form action="search.php" method="GET" class="main-form">
+      <label for="q" class="sr-only">What are you looking for?</label>
+      <input type="text" class="main-form__input" id="q" name="q" placeholder="What are you looking for?">
+
+      <button class="main-form__btn">Search</button>
+    </form>
   </section>
 
   <script src="assets/js/main-bundled.js"></script>
