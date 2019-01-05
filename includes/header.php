d9bbbcb6e2a42b4973c79e7b05c18d5f581750a1
Sai <saicharan13@gmail.com>
2019-01-05 15:36:16 +0530

    Add meta tags

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="Web Search Engine. Find what you are looking for.">
+  <meta name="keywords" content="bickle, web, images, search, results, search engine">
+  <meta name="author" content="Sai Charan">
   <title>
     <?=isset($_GET['q']) ? $_GET['q'] . " - Bickle Search" : "Bickle - Search the web"?>
   </title>
