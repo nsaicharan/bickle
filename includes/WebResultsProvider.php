66dcd971c770767df90504f91d44dfaaafb6c74c
Sai <saicharan13@gmail.com>
2019-01-07 15:16:47 +0530

    Display web search results

---
+++ includes/WebResultsProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+class WebResultsProvider 
+{
+  private $con;
+
+  public function __construct($con) {
+    $this->con = $con;
+  }
+
+  public function getNumResults($term)
+  {
+    $query = $this->con->prepare("SELECT COUNT(*) as total FROM sites 
+      WHERE title LIKE :term
+      OR url LIKE :term
+      OR keywords LIKE :term
+      OR description LIKE :term");
+    
+    $term = '%' . $term .'%';
+
+    $query->bindParam(":term", $term);
+    $query->execute();
+
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+
+    return $row['total'];
+  }
+
+  public function getResults($page, $pageSize, $term)
+  {
+    $query = $this->con->prepare("SELECT * FROM sites
+      WHERE title LIKE :term
+      OR url LIKE :term
+      OR keywords LIKE :term
+      OR description LIKE :term
+      ORDER BY clicks DESC");
+
+    $term = '%' . $term . '%';
+
+    $query->bindParam(":term", $term);
+    $query->execute();
+
+    $results = [];
+
+    while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+      // Limit the length of title and description
+      $row['title'] = $this->trimField($row['title'], 70);
+      $row['description'] = $this->trimField($row['description'], 200);
+
+      // Push row data into results array
+      $results[] = $row;
+    }
+
+    return $results;
+  }
+
+  private function trimField($string, $characterLimit)
+  {
+    $dots = strlen($string) > $characterLimit ? '...' : '';
+    
+    return substr($string, 0, $characterLimit) . $dots;
+  }
+}
+
--- search.php
+++ search.php
@@ -1,10 +1,13 @@
 <?php
-  // Check if query exists
+  require "config.php";
+  require "includes/WebResultsProvider.php";
+
+  // Check if search term (q) exists
   if (!isset($_GET['q']) || empty($_GET['q'])) {
     header("Location: /");
     exit;
   } else {
-    $query = $_GET['q'];
+    $term = $_GET['q'];
     $self = $_SERVER['PHP_SELF'];
     $type = "web";
 
@@ -24,21 +27,21 @@
       </div >
 
       <form role="search" action='<?= $self ?>' class="search-header__form">
-        <input type="text" class="search-header__input" name="q" value="<?= $query; ?>">
+        <input type="text" class="search-header__input" name="q" value="<?= $term; ?>">
         <button class="search-header__submit-btn">
           <img src="assets/img/search.svg" alt="search icon">
         </button>
       </form>
 
       <a 
-        href='<?= "$self?q=$query&type=web" ?>' 
+        href='<?= "$self?q=$term&type=web" ?>' 
         class="search-header__tab <?= ($type == 'web') ? 'search-header__tab--active' : '' ?>"
       >
         Web
       </a>
 
       <a 
-        href='<?= "$self?q=$query&type=images" ?>' 
+        href='<?= "$self?q=$term&type=images" ?>' 
         class="search-header__tab <?= ($type == 'images') ? 'search-header__tab--active' : '' ?>"
       >
         Images
@@ -46,5 +49,41 @@
     </div>
     <!-- /.container -->
   </header>
+
+  <section class="results">
+    <div class="container container--narrow">
+      <div class="results__count">
+        <?php
+          $resultsProvider = new WebResultsProvider($con);
+          $resultsCount = $resultsProvider->getNumResults($term);
+          echo "$resultsCount results found.";
+        ?>
+      </div>
+      <!-- /.results__count -->
+
+      <div class="results__data">
+        <?php 
+          $resultsArray = $resultsProvider->getResults(1, 20, $term);
+          
+          foreach ($resultsArray as $result) :
+        ?>
+          <div class="results__item">
+            <a href="<?= $result['url'] ?>" class="results__link">
+              <h2 class="results__title">
+                <?= $result['title'] ?>
+              </h2>
+
+              <cite class="results__url"><?= $result['url'] ?></cite>
+            </a>
+
+            <p class="results__description"><?= $result['description'] ?></p>
+          </div>
+          <!-- ./results__item -->
+        <?php endforeach; ?>
+      </div>
+      <!-- ./results__data -->
+    </div>
+    <!-- /.container -->
+  </section>
 
 <?php require "includes/footer.php" ?>
